1d72075a1af475c14b4977534f84c358da89db1b
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-07-07 22:10:45 -0300

    chore: add heroku route

--- src/services/socket/index.ts
+++ src/services/socket/index.ts
@@ -1,6 +1,8 @@
 import { createContext } from 'react';
 import { io } from 'socket.io-client';
 
-export const socket = io('localhost:3333/', { transports: ['websocket'] });
+export const socket = io('https://wilson-radio-server.herokuapp.com/', {
+  transports: ['websocket'],
+});
 
 export const SocketContext = createContext(socket);
