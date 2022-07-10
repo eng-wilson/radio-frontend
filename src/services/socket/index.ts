4ac1e835299ee968fac4fc67e9b1bc3ba0f32f64
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-07-10 14:28:54 -0300

    fix: environment variables names

--- src/services/socket/index.ts
+++ src/services/socket/index.ts
@@ -3,8 +3,8 @@ import { io } from 'socket.io-client';
 
 const apiURL =
   process.env.NODE_ENV === 'development'
-    ? process.env.API_DEV_URL
-    : process.env.API_PROD_URL;
+    ? process.env.REACT_APP_API_DEV_URL
+    : process.env.REACT_APP_API_PROD_URL;
 
 export const socket = io(apiURL || '', {
   transports: ['websocket'],
